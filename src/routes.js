0685512d764aada3ae65f94e6dd35a7a57021c24
Yan Garcia <yangarciavaz@gmail.com>
2021-04-02 17:51:24 -0300

    profile object created

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,19 @@ const routes = express.Router()
 
 const views = __dirname + "/views/"
 
+const profile = {
+  name: "Yan Garcia",
+  avatar: "https://avatars.githubusercontent.com/u/20269645?v=4",
+  "monthly-budget": 3000,
+  "days-per-week": 5,
+  "hours-per-day": 5,
+  "vacation-per-year": 4,
+}
+
 routes.get('/', (req, res) => res.render(views + "index"))
 routes.get('/job', (req, res) => res.render(views + "job"))
 routes.get('/job/edit', (req, res) => res.render(views + "job-edit"))
-routes.get('/profile', (req, res) => res.render(views + "profile"))
+routes.get('/profile', (req, res) => res.render(views + "profile", { profile }))
 
 
 
