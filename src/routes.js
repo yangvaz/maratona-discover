611fa204b23c312842059dc16339268ec855cb07
Yan Garcia <yangarciavaz@gmail.com>
2021-04-03 22:52:24 -0300

    refactoring jobs

--- src/routes.js
+++ src/routes.js
@@ -13,76 +13,85 @@ const profile = {
   "value-hour": 75,
 };
 
-const jobs = [
-  {
-    id: 1,
-    name: "Pizzaria Guloso",
-    "daily-hours": 2,
-    "total-hours": 60,
-    created_at: Date.now(),
+const Job = {
+  data: [
+    {
+      id: 1,
+      name: "Pizzaria Guloso",
+      "daily-hours": 2,
+      "total-hours": 1,
+      created_at: Date.now(),
+    },
+    {
+      id: 2,
+      name: "OneTwo Project",
+      "daily-hours": 3,
+      "total-hours": 47,
+      created_at: Date.now(),
+    },
+  ],
+
+  controllers: {
+    index(req, res) {
+      const updatedJobs = Job.data.map((job) => {
+        const remaining = Job.services.remainingDays(job);
+        const status = remaining <= 0 ? "done" : "progress";
+
+        return {
+          ...job,
+          remaining,
+          status,
+          budget: profile["value-hour"] * job["total-hours"],
+        };
+      });
+
+      return res.render(views + "index", { profile, jobs: updatedJobs });
+    },
+    create(req, res) {
+      res.render(views + "job")
+    },
+    save(req, res) {
+      // const lastId = jobs[jobs.length - 1]?.id || 1;  // atualizar pro node 14 e trocar pra este
+      const lastId = Job.data[Job.data.length - 1] ? jobs[jobs.length - 1].id : 1;
+
+      jobs.push({
+        id: lastId + 1,
+        name: req.body.name,
+        "daily-hours": req.body["daily-hours"],
+        "total-hours": req.body["total-hours"],
+        created_at: Date.now(),
+      });
+
+      return res.redirect("/");
+    },
   },
-  {
-    id: 2,
-    name: "OneTwo Project",
-    "daily-hours": 3,
-    "total-hours": 47,
-    created_at: Date.now(), 
+
+  services: {
+    remainingDays(job) {
+      const remainingDays = (job["total-hours"] / job["daily-hours"]).toFixed();
+
+      const createdDate = new Date(job.created_at);
+      const dueDay = createdDate.getDate() + Number(remainingDays);
+      const dueDate = createdDate.setDate(dueDay);
+
+      const timeDiffInMs = dueDate - Date.now();
+
+      // transform ms em days
+      const dayInMs = 1000 * 60 * 60 * 24;
+      const dayDiff = Math.floor(timeDiffInMs / dayInMs);
+
+      // restam xx dias
+      return dayDiff;
+    },
   },
-]
+};
 
-function remainingDays(job) {
-
-  const remainingDays = (job["total-hours"] / job["daily-hours"]).toFixed()
-
-  const createdDate = new Date(job.created_at)
-  const dueDay = createdDate.getDate() + Number(remainingDays)
-  const dueDate = createdDate.setDate(dueDay)
-
-  const timeDiffInMs = dueDate - Date.now()
-
-  // transform ms em days
-  const dayInMs = 1000 * 60 * 60 * 24
-  const dayDiff = Math.floor(timeDiffInMs / dayInMs)
-
-  // restam xx dias
-  return dayDiff
-}
-
-routes.get("/", (req, res) => {
-  
-  const updatedJobs = jobs.map((job) => {
-    
-   const remaining = remainingDays(job)
-   const status = remaining <= 0 ? 'done' : 'progress'
-
-    return {
-      ...job,
-      remaining,
-      status,
-      budget: profile["value-hour"] * job["total-hours"]
-    }
-  })
-  
-  res.render(views + "index", { profile, jobs: updatedJobs })
-  
-});
-routes.get("/job", (req, res) => res.render(views + "job"));
-routes.post("/job", (req, res) => {
-
-  // const lastId = jobs[jobs.length - 1]?.id || 1;  // atualizar pro node 14 e trocar pra este
-  const lastId = jobs[jobs.length - 1] ? jobs[jobs.length - 1].id : 1;
-
-  jobs.push({
-    id: lastId + 1,
-    name: req.body.name,
-    "daily-hours": req.body["daily-hours"],
-    "total-hours": req.body["total-hours"],
-    created_at: Date.now()
-  })
-
-  return res.redirect('/')
-});
+routes.get("/", Job.controllers.index);
+routes.get("/job", Job.controllers.create);
+routes.post("/job", Job.controllers.save);
 routes.get("/job/edit", (req, res) => res.render(views + "job-edit"));
-routes.get("/profile", (req, res) => res.render(views + "profile", { profile }));
+routes.get("/profile", (req, res) =>
+  res.render(views + "profile", { profile })
+);
 
 module.exports = routes;
