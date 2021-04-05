7eba67ea60df74bf79029fdd8f5e6d26e8e47a85
Yan Garcia <yangarciavaz@gmail.com>
2021-04-04 22:03:14 -0300

    created functions: show and update

--- src/routes.js
+++ src/routes.js
@@ -68,7 +68,7 @@ const Job = {
           ...job,
           remaining,
           status,
-          budget: Profile.data["value-hour"] * job["total-hours"],
+          budget: Job.services.calculateBudget(job, Profile.data["value-hour"]),
         };
       });
 
@@ -93,6 +93,48 @@ const Job = {
 
       return res.redirect("/");
     },
+    show(req, res) {
+
+      const jobId = req.params.id
+
+      const job = Job.data.find(job => Number(job.id) === Number(jobId))
+
+      if(!job) {
+        return res.send('Job not found!')
+      }
+
+      job.budget = Job.services.calculateBudget(job, Profile.data["value-hour"])
+
+      return res.render(views + "job-edit", {job})
+    },
+    update(req, res) {
+      const jobId = req.params.id
+
+      const job = Job.data.find(job => Number(job.id) === Number(jobId))
+
+      if(!job) {
+        return res.send('Job not found!')
+      }
+
+      const updatedJob = {
+        ...job,
+        name: req.body.name,
+        "total-hours": req.body["total-hours"],
+        "daily-hours": req.body["daily-hours"],
+      }
+
+      Job.data = Job.data.map(job => {
+
+        if(Number(job.id) === Number(jobId)) {
+          job = updatedJob
+        }
+
+        return job
+      })
+
+      res.redirect('/job/' + jobId)
+
+    }
   },
 
   services: {
@@ -112,13 +154,15 @@ const Job = {
       // restam xx dias
       return dayDiff;
     },
+    calculateBudget: (job, valueHour) => valueHour * job["total-hours"]
   },
 };
 
 routes.get("/", Job.controllers.index);
 routes.get("/job", Job.controllers.create);
 routes.post("/job", Job.controllers.save);
-routes.get("/job/edit", (req, res) => res.render(views + "job-edit"));
+routes.get("/job/:id", Job.controllers.show);
+routes.post("/job/:id", Job.controllers.update);
 routes.get("/profile", Profile.controllers.index);
 routes.post("/profile", Profile.controllers.update);
 
